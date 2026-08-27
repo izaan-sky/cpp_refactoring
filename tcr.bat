9b65a44bd6e483a4cf39bac45c4522f240c935e5
Jason Gorman <jason.gorman@codemanship.com>
2026-08-27 12:06:40 +0100

    Changed tcr.bat to call test.bat

--- tcr.bat
+++ tcr.bat
@@ -1,2 +1,2 @@
 @echo off
-cmake -S . -B build -G "MinGW Makefiles" && cmake --build build && ctest --test-dir build --output-on-failure && (git add -A && git commit -m "TCR: tests passing") || (git checkout -- . && git clean -fd)
+call test.bat && (git add -A && git commit -m "TCR: tests passing") || (git checkout -- . && git clean -fd)
