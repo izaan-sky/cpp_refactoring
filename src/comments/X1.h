befb79b8d2488c8bba9a733d646e0ad6b32dd137
Izaan Siddiqi <izaan.siddiqi@sky.uk>
2026-09-02 10:43:29 +0100

    TCR: tests passing

--- src/comments/X1.h
+++ src/comments/X1.h
@@ -4,7 +4,7 @@ namespace refactoring::comments {
 
 class X1 {
 public:
-    static int sumSquaresInRange(int q, int z);
+    static int sumSquaresInRange(int lowerBound, int z);
     static int square(int k);
 };
 
