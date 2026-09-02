c14d0eb0606476c88914b787af5c22b9b9718f34
Izaan Siddiqi <izaan.siddiqi@sky.uk>
2026-09-02 10:51:22 +0100

    TCR: tests passing

--- tests/comments/X1Test.cpp
+++ tests/comments/X1Test.cpp
@@ -8,7 +8,6 @@ TEST(MathematicalOperationsTest, sumsSquaresFrom7To12) {
     int a = 7;
     int b = 12;
 
-    // Expected: sum of squares from 7 to 12
     int expected = 0;
     for (int i = a; i <= b; i++) {
         expected += i * i;
