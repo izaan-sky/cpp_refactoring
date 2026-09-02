e816eb4671407960c3f1b6c70c958a9047161a85
Lam, Alvin (Engineer - Software Engineering - Sky) <alvin.lam@sky.uk>
2026-09-02 10:32:06 +0100

    TCR: tests passing

--- src/comments/X1.cpp
+++ src/comments/X1.cpp
@@ -2,11 +2,11 @@
 
 namespace refactoring::comments {
 
-int X1::m(int q, int z) {
+int X1::m(int lowerBound, int z) {
     int p = 0;
 
     // Iterate from lower bound (q) to upper bound (z)
-    for (int i = q; i <= z; i++) {
+    for (int i = lowerBound; i <= z; i++) {
         // Add square of each number in the range
         p += n(i);
     }
