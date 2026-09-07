3cdf120fe8c33469c9a020b62b2828d3db2d2ac7
Izaan Siddiqi <izaan.siddiqi@sky.uk>
2026-09-07 11:35:39 +0100

    TCR: tests passing

--- src/divergentchange/CustomerService.cpp
+++ src/divergentchange/CustomerService.cpp
@@ -1,7 +1,9 @@
 #include "divergentchange/CustomerService.h"
+
+#include "divergentchange/AccountStatus.h"
+#include "divergentchange/DisplayName.h"
 #include "divergentchange/EmailValidation.h"
 #include "divergentchange/StringManipulation.h"
-#include "divergentchange/AccountStatus.h"
 
 #include <cctype>
 
