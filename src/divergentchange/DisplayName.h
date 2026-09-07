699825000e661d55328d6a030544df848cc4bbbc
Izaan Siddiqi <izaan.siddiqi@sky.uk>
2026-09-07 11:29:49 +0100

    TCR: tests passing

---
+++ src/divergentchange/DisplayName.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+class DisplayName
+{
+public:
+    std::string format(const std::string& firstName, const std::string& lastName) const {
+        return {};
+    }
+};
