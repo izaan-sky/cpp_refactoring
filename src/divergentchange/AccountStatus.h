3fb18f9d9185939b3612a956129ac18c81a05116
Lam, Alvin (Engineer - Software Engineering - Sky) <alvin.lam@sky.uk>
2026-09-07 10:53:47 +0100

    TCR: tests passing

--- src/divergentchange/AccountStatus.h
+++ src/divergentchange/AccountStatus.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <string>
+
 class AccountStatus {
-    
+    public: 
+        static std::string determineAccountStatus(int daysSinceLastLogin) {
+            if (daysSinceLastLogin > 365) {
+                return "INACTIVE";
+            } else if (daysSinceLastLogin > 30) {
+                return "DORMANT";
+            }
+            return "ACTIVE";
+        }
 };
